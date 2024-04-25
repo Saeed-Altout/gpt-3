21429ce2385f473004370b4515cf107b79a4dff5
Saeed-Altout <saeedaltoutpro@gmail.com>
2024-04-25 20:49:29 +0300

    setup style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,11 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: "#FF4820",
+        background: "#040c18",
+        foreground: "#042C54",
+      },
     },
   },
   plugins: [],
